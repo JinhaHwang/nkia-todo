c40b6951dcd88bbdb63e46d7469b565dc69b138f
황진하 <gojinha@naver.com>
2019-04-25 11:31:42 +0900

    걍 다 추가

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import nkia from './nkia.png';
 import './App.css';
+import TodoHeader from "./TodoHeader";
+import TodoContents from "./TodoContents";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            myMessage : '',
+            list : [],
+            checkNum : 0
+        }
+        /*
+        list = [
+            { todoText: '할 일', index: 0, complete: true },
+            { todoText: '할 일2', index: 1, complete: false },
+            { todoText: '할 일3', index: 2, complete: false },
+        ]
+         */
+    }
+
+    inputValue(value) {
+        this.setState({
+                myMessage: value
+            }
+        )
+    }
+    addInputValue(value) {
+        this.setState((state) => {
+            return {
+                list: state.list.concat(value),
+                myMessage: ''
+            }
+        });
+
+
+    }
+    handleTodoRemove = (val) => {
+        this.setState((state) => {
+            return {
+                list: state.list.filter(todo => todo !== val)
+            }
+        });
+
+    }
+    handleTodoComplete = (val) => {
+        this.setState((state) => {
+            return {
+                list : state.list.filter(todo => todo !== val),
+                checkNum : state.checkNum + 1
+            }
+        })
+    }
+
+    // arrowFunction = (인자) => 반환
+    handleTodoChange = (e) => this.inputValue(e.target.value)
+
+    handleTodoAdd = () => this.addInputValue(this.state.myMessage)
+
+    render() {
+        return (
+            <div className="todo_list">
+
+                <TodoHeader logo={nkia} todoText={this.state.myMessage} onTodoChange={this.handleTodoChange} onTodoAdd={this.handleTodoAdd} />
+                <TodoContents todoItems={this.state.list} onTodoComplete={this.handleTodoComplete} onTodoRemove={this.handleTodoRemove}
+                              completeCount={this.state.checkNum}/>
+            </div>
+        );
+    }
+
+
+
 }
 
 export default App;
+
